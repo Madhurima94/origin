d59d57386b648f5fcbca483e45609d2fdee8ce25
Madhurima Das <madhurima@promact.com>
2017-02-21 22:56:29 -0800

    Added Modified Assignment of ORM

--- assignments/ORM/ORMDEMO/src/ORMDEMO/Program.cs
+++ assignments/ORM/ORMDEMO/src/ORMDEMO/Program.cs
@@ -13,17 +13,19 @@ namespace ORMDEMO
             using (var db =new Model())
             {
                 db.Database.Migrate();
-                /*  db.Products.Add(new ProductTable
+                  db.Products.Add(new ProductTable
                   {
                       ProductName="Mobile Phones",
                       ProductDescription="Mobile Phones are Android types"
                   });
+                var count1 = db.SaveChanges();
                   db.Products.Add(new ProductTable
                   {
                       ProductName="Computers",
 
 
                   });
+                var count2 = db.SaveChanges();
                   db.Products.Add(new ProductTable
                   {
                       ProductName = "Laptops",
@@ -31,40 +33,55 @@ namespace ORMDEMO
 
 
                   });
+                var count3 = db.SaveChanges();
                   db.Updates.Add(new UpdateTable
                   {
                       UpdateName = "Android Phones",
-                      PID=1
+                      PID=4
 
                   });
+                var count4 = db.SaveChanges();
 
-                 db.Products.Update(new ProductTable
+                db.Updates.Add(new UpdateTable
+                {
+                    UpdateName = "Nokia Phones",
+                    PID = 4
+
+                });
+
+                var count9 = db.SaveChanges();
+                db.Products.Update(new ProductTable
                   {
-                     PID=1,
+                     PID=3,
                      ProductName="Laptops"
                   });
 
+                var count5 = db.SaveChanges();
                   db.Updates.Update(new UpdateTable
                   {
-                      UpdateID = 1,
-                      UpdateName = "Nokia Phones",
-                      PID=1
+                      UpdateID = 9,
+                      UpdateName = "Lenovo Phones",
+                      PID=5
 
                   });
+                var count6 = db.SaveChanges();
 
                   db.Products.Remove(new ProductTable
                   {
-                      PID = 1
+                      PID = 5
                   });
+
+                var count7 = db.SaveChanges();
                 db.Updates.Add(new UpdateTable
                 {
                     UpdateName = "4G Phones",
                     PID = 3
 
-                });*/
+                });
+                var count8 = db.SaveChanges();
                 db.Updates.Remove(new UpdateTable
                 {
-                    UpdateID = 7,
+                    UpdateID = 17,
                     PID = 3
                 });
 
